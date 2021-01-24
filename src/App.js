aaa0e060ffa1f186927570796589b8feb2c2721a
Maxim Shirshin <maxshirshin@gmail.com>
2021-01-24 21:30:55 +0100

    App adjustments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./organisms/nav";
+import './components/content/index.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.wayfair.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Wayfair
-        </a>
-      </header>
+    <div className="b-app">
+      <Nav/>
+      <div className="b-content">Here the text goes</div>
     </div>
   );
 }
